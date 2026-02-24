962d35222344a418bbeb0a38931125a2365450f0
bnenovP <bnenov91@gmail.com>
2026-02-24 21:25:21 +0200

    Created repository layer

---
+++ src/main/java/com/example/baranclinic/clinical/domain/repository/ProviderRepository.java
@@ -0,0 +1,14 @@
+package com.example.baranclinic.clinical.domain.repository;
+
+import com.example.baranclinic.clinical.domain.entity.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProviderRepository extends JpaRepository<Provider, UUID> {
+
+    Optional<Provider> findByLicenseNumber(String licenseNumber);
+}
