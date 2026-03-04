7d64f9e190609be7260ef458da455cc933979127
bnenovP <bnenov91@gmail.com>
2026-03-04 12:14:58 +0200

    Removed unused import statements

--- src/main/java/com/example/baranclinic/clinical/domain/dto/response/ProviderResponseDTO.java
+++ src/main/java/com/example/baranclinic/clinical/domain/dto/response/ProviderResponseDTO.java
@@ -1,14 +1,8 @@
 package com.example.baranclinic.clinical.domain.dto.response;
 
-import com.example.baranclinic.clinical.domain.entity.MedicalRecordEntry;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.Value;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
