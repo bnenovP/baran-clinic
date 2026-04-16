f19fcc4ae045f480bc736b0accb59b43d91cf2ab
bnenovP <bnenov91@gmail.com>
2026-04-16 15:56:42 +0300

    Update AbstractIntegrationTest.java

--- src/test/java/com/example/baranclinic/AbstractIntegrationTest.java
+++ src/test/java/com/example/baranclinic/AbstractIntegrationTest.java
@@ -6,7 +6,9 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class AbstractIntegrationTest {
