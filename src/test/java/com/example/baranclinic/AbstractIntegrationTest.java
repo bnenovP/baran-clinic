5ee9a5a6da76dcc03c60725d25528ff5be614fd7
bnenovP <bnenov91@gmail.com>
2026-04-16 15:58:10 +0300

    Update AbstractIntegrationTest.java

--- src/test/java/com/example/baranclinic/AbstractIntegrationTest.java
+++ src/test/java/com/example/baranclinic/AbstractIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.example.baranclinic;
 
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
